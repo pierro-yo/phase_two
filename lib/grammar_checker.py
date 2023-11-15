deaf8e7d86efa3cfea1c20a9338216269a9a7b69
Piers Ford <piers.ford@hotmail.co.ukl>
2023-11-15 13:55:34 +0000

    Added grammar checker and test and to do checker and tests

---
+++ lib/grammar_checker.py
@@ -0,0 +1,17 @@
+
+def capital_first_letter(string):
+    user_string = string
+    if user_string[0].isupper():
+        return "Grammar is ok"
+    else:
+        return "capital letter needed"
+# print(capital_first_letter('Hello'))
+
+def sentence_ending(string):
+    special_chars = ".!?"
+    user_string = string[-1]
+    if any(char in special_chars for char in user_string):
+        return "grammar is correct"
+    else:
+        return "need either ! ? ."
+print(sentence_ending("hello."))
---
+++ tests/test_grammar_checker.py
@@ -0,0 +1,9 @@
+from lib.grammar_checker import *
+
+def test_capital_first_letter():
+    test_string = "Hello"
+    assert capital_first_letter(test_string) == "Grammar is correct"
+
+def test_sentence_ending():
+    test_string = "hello!"
+    assert sentence_ending(test_string) == "grammar is correct"
